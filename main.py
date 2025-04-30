5c2296384035e90e58dc27d02d7f5c3bd5bf0397
arafatDU <bsse1435@iit.du.ac.bd>
2025-04-30 18:36:39 +0600

    Creating CRUD api

--- main.py
+++ main.py
@@ -1,15 +1,46 @@
-from typing import Union
-
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+
 
 app = FastAPI()
+
+class Tea(BaseModel):
+    id: int
+    name: str
+    origin: str
+    
+    
+teas: List[Tea] = []
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"message": "Hello, World!"}
+
+@app.get("/teas")
+def get_teas():
+    return teas
+
+@app.post("/teas")
+def create_tea(tea: Tea):
+    teas.append(tea)
+    return tea
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+app.put("/teas/{tea_id}")
+def update_tea(tea_id: int, updated_tea: Tea):
+    for i, t in enumerate(teas):
+        if t.id == tea_id:
+            teas[i] = updated_tea
+            return updated_tea
+    return {"error": "Tea not found"}
+
+
+@app.delete("/teas/{tea_id}")
+def delete_tea(tea_id: int):
+    for index, t in enumerate(teas):
+        if t.id == tea_id:
+            teas.pop(index)
+            return {"message": "Tea deleted"}
+    return {"error": "Tea not found."}
